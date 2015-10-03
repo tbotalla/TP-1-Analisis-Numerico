f1846c031e76eef66179116b53bc78cb8c9c1abd
tbotalla <14.tomas@gmail.com>
2015-10-03 02:13:27 -0300

    Commit inicial

---
+++ lagrangeBaricentrico.m
@@ -0,0 +1,20 @@
+
+% xPuntos = vector fila conteniendo los valores x dato
+% yPuntos = vector fila conteniendo los valores y dato
+% x = valor en el que se evalua el polinomio
+% cantPuntos = numero de datos
+function L = lagrangeBaricentrico(xPuntos,yPuntos,cantPuntos,x)
+
+	
+	
+%Calculo de los pesos baricentricos
+	n = length(cantPuntos);
+	
+	
+	for k=0:n
+		
+	end
+	
+	
+
+end
