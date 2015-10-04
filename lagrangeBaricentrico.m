6b2ad441eb0e9210bf67e26d51bd01b9692796aa
tbotalla <14.tomas@gmail.com>
2015-10-04 07:31:15 -0300

    cambiados comments

--- lagrangeBaricentrico.m
+++ lagrangeBaricentrico.m
@@ -1,19 +1,19 @@
-% xPuntos = vector fila conteniendo los valores x dato
-% yPuntos = vector fila conteniendo los valores y dato
-% x = valor en el que se evalua el polinomio
+# xPuntos = vector fila conteniendo los valores x dato
+# yPuntos = vector fila conteniendo los valores y dato
+# x = valor en el que se evalua el polinomio
 function Pn = lagrangeBaricentrico(xPuntos,yPuntos,x)
-	Pn = 0; %polinomio evaluado en el x dado para ser devuelto por la funcion
+	Pn = 0;  #polinomio evaluado en el x dado para ser devuelto por la funcion
 	n = length(xPuntos); %numero de datos
 
-	%L(x)
+	#L(x)
 	Lx = 1;
 	for i=1:n
 		Lx = Lx * (x - xPuntos(i));
 	end
 
-	%Pn(x)
+	#Pn(x)
 	for i=1:n
-		%Calculo del peso baricentrico de la iteracion correspondiente
+		#Calculo del peso baricentrico de la iteracion correspondiente
 		Wi = 1;
 		for k=1:n
 			if(k != i)
@@ -26,9 +26,9 @@ function Pn = lagrangeBaricentrico(xPuntos,yPuntos,x)
 end
 
 
-%ejemplo con el que lo probe; 
-% xPuntos = [ 1 2 3 4 ]
-% yPuntos = [ 1 2 3 4 ]
-% lagrangeBaricentrico(xPuntos,yPuntos,5)
-% la interpolacion devuelve Pn(x=5) = 5
+#ejemplo con el que lo probe; 
+# xPuntos = [ 1 2 3 4 ]
+# yPuntos = [ 1 2 3 4 ]
+# lagrangeBaricentrico(xPuntos,yPuntos,5)
+# la interpolacion devuelve Pn(x=5) = 5
 
