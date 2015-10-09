2c18bb5b232ce1be79948ed95eb112cc95bb0add
Tomas Botalla <tbotalla@localhost.localdomain>
2015-10-09 13:56:46 -0300

    solucionado problema de division por cero en lagrange baricentrico

--- TP1.m
+++ TP1.m
@@ -3,6 +3,23 @@ function retorno = TP1()
 
 tensiones = [ 0 20 40 60 80 100 120 140 160 180 190 200 210 220 230 240 250 ];
 deformaciones = [ 0.0000001 0.00028 0.0005771 0.0008486 0.001155 0.001464 0.001795 0.002427 0.004329 0.01058 0.01899 0.03068 0.05582 0.09165 0.1638 0.2498 0.4484 ]
+##
+##
+##Ejercicio 1: Lagrange Baricentrico
+
+xAInterpolar = 0;#(en que punto la interpolacion??)
+lagrangeBaricentrico(tensiones, deformaciones, xAInterpolar);
+
+##
+##
+##Ejercicio 2: Spline
+#la tenemos que implementar nosotros a la spline??
+
+##
+##
+##Ejercicio 3: Ramberg-Osgood
+#a)
+
 
 
 
--- lagrangeBaricentrico.m
+++ lagrangeBaricentrico.m
@@ -1,14 +1,14 @@
-# xPuntos = vector fila conteniendo los valores x dato
-# yPuntos = vector fila conteniendo los valores y dato
+# tensiones = vector fila conteniendo los valores x dato
+# deformaciones = vector fila conteniendo los valores y dato
 # x = valor en el que se evalua el polinomio
-function Pn = lagrangeBaricentrico(xPuntos,yPuntos,x)
+function Pn = lagrangeBaricentrico(tensiones,deformaciones,x)
 	Pn = 0;  #polinomio evaluado en el x dado para ser devuelto por la funcion
-	n = length(xPuntos); %numero de datos
+	n = length(tensiones); %numero de datos
 
 	#L(x)
 	Lx = 1;
 	for i=1:n
-		Lx = Lx * (x - xPuntos(i));
+		Lx = Lx * (x - tensiones(i));
 	end
 
 	#Pn(x)
@@ -17,18 +17,28 @@ function Pn = lagrangeBaricentrico(xPuntos,yPuntos,x)
 		Wi = 1;
 		for k=1:n
 			if(k != i)
-				Wi = Wi * (1 / (xPuntos(i) - xPuntos(k))); 
+				Wi = Wi * (1 / (tensiones(i) - tensiones(k)));
 			endif
 		end
-		
-		Pn = Pn + yPuntos(i) * ((Lx * Wi) / (x - xPuntos(i)));
+
+		#Verificar que funciona bien cuando X = Xi o en este caso x = tensiones(i)
+		if (x == tensiones(i)) ##con esto se salva la condicion x == tensiones(i) que provoca division por cero
+			Pn = Pn + deformaciones(i);
+		else
+			Pn = Pn + deformaciones(i) * ((Lx * Wi) / (x - tensiones(i)));
+		end
 	end
 end
 
 
-#ejemplo con el que lo probe; 
-# xPuntos = [ 1 2 3 4 ]
-# yPuntos = [ 1 2 3 4 ]
-# lagrangeBaricentrico(xPuntos,yPuntos,5)
+#ejemplo con el que lo probe;
+# tensiones = [ 1 2 3 4 ]
+# deformaciones = [ 1 2 3 4 ]
+# lagrangeBaricentrico(tensiones,deformaciones,5)
 # la interpolacion devuelve Pn(x=5) = 5
 
+#otro ejemplo
+#tensiones = [ 0 20 40 60 80 100 120 140 160 180 190 200 210 220 230 240 250 ]
+#deformaciones = [ 0.0000001 0.00028 0.0005771 0.0008486 0.001155 0.001464 0.001795 0.002427 0.004329 0.01058 0.01899 0.03068 0.05582 0.09165 0.1638 0.2498 0.4484 ]
+#lagrangeBaricentrico(tensiones,deformaciones,120)
+#la interpolacion devuelve Pn(x=120) = 0.0017950
