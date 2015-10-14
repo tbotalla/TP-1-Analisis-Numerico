66579f9674dda97c290c85d080a600c11592e0e8
Tomas Botalla <tbotalla@localhost.localdomain>
2015-10-14 20:16:33 -0300

    modificado CM para el 3b

--- ejercicio3b.m
+++ ejercicio3b.m
@@ -1,14 +1,15 @@
 #utiliza los ultimos 10 datos y el valor de E obtenido en la parte 3)a)
-#para hacer una aproximacion por cuadrados minimos de ln(A) = 0.002/((f0,2)^n) y B = n 
+#para hacer una aproximacion por cuadrados minimos de ln(A) = 0.002/((f0,2)^n) y B = n
 function [lnA,B] = ejercicio3b(tensiones,deformaciones,E)
 
-sumaX = 0;
-cuadradoSumaX = 0;
-sumaXCuadrado = 0;
+sumaLnX = 0;
+cuadradoSumaLnX = 0;
+sumaLnXCuadrado = 0;
 for i=(length(tensiones)-10):length(tensiones)
-  sumaX = sumaX + tensiones(i);
-  sumaXCuadrado = sumaXCuadrado + (tensiones(i) * tensiones(i));
-  cuadradoSumaX = (sumaX * sumaX);
+  sumaLnX = sumaLnX + log(tensiones(i));
+  lnXCuadrado = log(tensiones(i)) * log(tensiones(i));
+  sumaLnXCuadrado = sumaLnXCuadrado + lnXCuadrado;
+  cuadradoSumaLnX = (sumaLnX * sumaLnX);
 endfor
 
 sumaLnY = 0;
@@ -19,21 +20,26 @@ endfor
 producto = 1;
 sumaProducto = 0;
 for i=(length(tensiones)-10):length(tensiones)
-  producto = tensiones(i) * log(deformaciones(i));
+  producto = log(tensiones(i)) * log(deformaciones(i));
   sumaProducto = sumaProducto + producto;
 endfor
 
-B = (10 * sumaProducto - (sumaX * sumaLnY)) / ((10 * sumaXCuadrado) - (cuadradoSumaX));
-lnA = ((sumaXCuadrado * sumaLnY) - (sumaProducto * sumaX)) / ((10 * sumaXCuadrado) - (cuadradoSumaX));
+B = (10 * sumaProducto - (sumaLnX * sumaLnY)) / ((10 * sumaLnXCuadrado) - (cuadradoSumaLnX));
+lnA = ((sumaLnXCuadrado * sumaLnY) - (sumaProducto * sumaLnX)) / ((10 * sumaLnXCuadrado) - (cuadradoSumaLnX));
 #hay que agregar que devuelva f02 para el ejercicio 5
 
 ##################################
 #Grafico con los valores obtenidos
-xAGraficar = tensiones;
-for i=1:length(tensiones)
+#xAGraficar = tensiones;
+xAGraficar = [ 140 160 180 190 200 210 220 230 240 250 ];
+#xAGraficar
+for i=1:length(xAGraficar)
     yAGraficar(i) = exp(lnA + B * log(tensiones(i))) + (tensiones(i) / E);
+    #yAGraficar(i) = (lnA + B * log(tensiones(i))) ;
 end
+#yAGraficar
 title('Cuadrados Minimos');
 plot(xAGraficar, yAGraficar,'-','MarkerEdgeColor','r','LineWidth',0.5); #grafica en color rojo, con ancho de linea=0.5);
+
 
 endfunction
