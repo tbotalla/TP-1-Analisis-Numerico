7a3c504ce46521ee0548c8354e306e46706419b6
root <root@localhost.localdomain>
2015-10-15 12:23:21 -0300

    arreglado detalle del grafico 3c

--- ejercicio3c.m
+++ ejercicio3c.m
@@ -3,11 +3,14 @@ function [ret] = ejercicio3c(tensiones,deformaciones,E,lnA,B,f02)
 
   n = length(tensiones);
   xAGraficar = tensiones;
-  for i=1:n-1;
-      yAGraficar = (tensiones(i) / E) + 0.002 * realpow((tensiones(i)/f02),B);
+  for i=1:n;
+      #xAGraficar = tensiones(i):0.1:tensiones(i+1);
+      yAGraficar(i) = (tensiones(i) / E) + 0.002 * realpow((tensiones(i)/f02),B);
       hold on;
   end
-  
+  #xAGraficar
+  #yAGraficar
+
   title('Expresion de Ramberg-Osgood');
   plot(xAGraficar,yAGraficar,'-','MarkerEdgeColor','r'); #grafica en color rojo, con ancho de linea=0.5
 
